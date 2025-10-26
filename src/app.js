6b4812a062e44e4e3beefc1ffdfdb314e44cd8c7
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-10-26 05:43:07 +0300

    full user CRUD + Authentication

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const routes = require("./routes");  // 👈 imports index.js automatically
+const userRouter=require("./routes/userRoutes");
 
 dotenv.config();
 connectDB();
@@ -11,6 +12,8 @@ app.use(express.json());
 
 // Base API prefix
 app.use("/api", routes);
+
+app.use(`/api/users`,userRouter);
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce");
+        const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/ecommerce", {
+     family: 4,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
