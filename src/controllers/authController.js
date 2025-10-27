0be0e406544ee99cedaa5c942060faaa87c42fd8
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-10-27 23:35:59 +0300

    authrization(log in first,admin or user) plus orders(controller,model,routes)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
-const User=require('./../models/UserModel')
+const AppError = require('../utils/appError')
+const User=require('./../models/userModel')
 const catchAsync=require('./../utils/catchAsync')
 const jwt=require('jsonwebtoken')
+const {promisify}=require('util')
 
 const signToken=id=>{
     return jwt.sign({
@@ -9,12 +11,14 @@ const signToken=id=>{
     process.env.JWT_SECRET,{expiresIn:process.env.JWT_EXPIRES_IN})
 }
 
-signup=catchAsync(async (req,res,next)=>{
+const signup=catchAsync(async (req,res,next)=>{
         const newUser=await User.create({
             name:req.body.name,
             email:req.body.email,
             password:req.body.password,
-            passwordConfirm:req.body.passwordConfirm
+            passwordConfirm:req.body.passwordConfirm,
+            role:req.body.role,
+           passwordChangeAt:req.body.passwordChangeAt
         })
         const token=signToken(newUser._id)
         res.status(201).json({
@@ -24,6 +28,7 @@ signup=catchAsync(async (req,res,next)=>{
                 user:newUser,
             }
         })
+
 })
 
 const login=catchAsync(async (req,res,next)=>{
@@ -54,7 +59,54 @@ const login=catchAsync(async (req,res,next)=>{
 
 })
 
+const protectLogOnly=catchAsync(async (req,res,next)=>{
+    
+ let token;
+  if(req.headers.authorization&&req.headers.authorization.startsWith('Bearer')){
+    token=req.headers.authorization.split(' ')[1];
+      
+  }
+  //console.log(token)
+  if(!token){
+     return  res.status(401).json({
+      status: 'you are not logged in please log in to get access.',
+    })
+  }
+
+const decoded = await promisify(jwt.verify)(token,process.env.JWT_SECRET)
+console.log(decoded)
+ 
+const freshUser=await User.findById(decoded.id)
+if(!freshUser){
+    return next(
+        new AppError("the user is no longer exist",401)
+    )
+}
+console.log(freshUser)
+
+ if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(new AppError('User recently changed password! Please log in again.', 401));
+  }
+req.user=freshUser
+next()
+
+
+
+
+}
+)
+const restrictTo=(...roles)=>{
+        return (req,res,next)=>{
+            if(!roles.includes(req.user.role)){
+                return next(new AppError('tou donot have permissino to delete',403))
+            }
+            next()
+        }
+}
+
 module.exports={
 signup,
-login
+login,
+protectLogOnly,
+restrictTo
 }
--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,1 +1,23 @@
  
+const express = require('express');
+const {
+  createOrder,
+  getMyOrders,
+  getAllOrders,
+  updateOrderStatus
+} = require('../controllers/orderController');
+const { protectLogOnly, restrictTo } = require('../controllers/authController');
+
+const router = express.Router();
+
+router.use(protectLogOnly); 
+
+router.route('/')
+  .get(restrictTo('admin'), getAllOrders)
+  .post(createOrder);
+
+router.route('/myOrders').get(getMyOrders);
+
+router.route('/:id').patch(restrictTo('admin'), updateOrderStatus);
+
+module.exports = router;
