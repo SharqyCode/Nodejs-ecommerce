8b1aff8edfe90626387e98f5d3f7ca181412d5c9
Ebraheem <pencilmagicart@gmail.com>
2025-10-27 17:33:24 +0300

    pushing

--- src/app.js
+++ src/app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const routes = require("./routes");  // 👈 imports index.js automatically
-const userRouter=require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
+const cors = require("cors")
 
 dotenv.config();
-connectDB();
+// connectDB();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 // Base API prefix
 app.use("/api", routes);
 
-app.use(`/api/users`,userRouter);
+app.use(`/api/users`, userRouter);
 
 const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
