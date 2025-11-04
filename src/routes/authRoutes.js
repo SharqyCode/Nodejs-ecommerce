1fc3933a20367cfabd1bc356ffce5336effb31db
abdelrhmanzenhom <73444167+abdelrhmanzenhom@users.noreply.github.com>
2025-11-04 02:37:28 +0200

    problems

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -36,14 +36,14 @@ passport.deserializeUser(async (id, done) => {
   done(null, user);
 });
 
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'],prompt : "select_account" }));
 
 router.get('/google/callback',
   passport.authenticate('google', { session: false, failureRedirect: '/login' }),
   (req, res) => {
 
     const token = jwt.sign({ id: req.user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
-    res.json({ status: 'success', token });
+   res.redirect(`http://localhost:5174/auth/oauth-success?token=${token}`);
   }
 );
 
